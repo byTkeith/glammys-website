52c37ac508d11e9b7cb2f6fe2c78473b2b30ebc2
Tendai Nyevedzanai <keithsolo.sav@gmail.com>
2025-12-08 19:09:25 +0200

    added .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.API_KEY),
+    },
+    build: {
+      outDir: 'dist',
+    }
+  };
 });
